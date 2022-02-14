822a286b7d5666e35a0c00c98762750b5df51888
ltaoo <litaowork@aliyun.com>
2022-02-14 21:39:16 +0800

    refactor: exec schedule at least once.

---
+++ domains/paragraph/constants.ts
@@ -0,0 +1,109 @@
+const definite = [
+  {
+    text: "all",
+  },
+  {
+    text: "both",
+  },
+  {
+    text: "half",
+  },
+  {
+    text: "double",
+  },
+  {
+    text: "twice",
+  },
+  {
+    text: "one-third",
+  },
+  {
+    text: "such",
+  },
+  /* 中位限定词 */
+  // 冠词
+  {
+    text: "a",
+  },
+  {
+    text: "an",
+  },
+  {
+    text: "the",
+  },
+  // 指示限定词
+  {
+    text: "this",
+  },
+  {
+    text: "that",
+  },
+  {
+    text: "these",
+  },
+  {
+    text: "those",
+  },
+  // 不定限定词
+  {
+    text: "some",
+  },
+  {
+    text: "any",
+  },
+  {
+    text: "either",
+  },
+  // 否定限定词
+  {
+    text: "no",
+  },
+  {
+    text: "neither",
+  },
+  // 全称限定词
+  {
+    text: "every",
+  },
+  {
+    text: "each",
+  },
+  // 所有格
+  {
+    text: "my",
+  },
+  {
+    text: "your",
+  },
+  {
+    text: "their",
+  },
+  {
+    text: "John's",
+  },
+  /* 后位限定词 */
+  // 数字
+  {
+    text: "three",
+  },
+  // 序数
+  {
+    text: "seventh",
+  },
+  {
+    text: "last",
+  },
+  // 表示多少
+  {
+    text: "many",
+  },
+  {
+    text: "much",
+  },
+  {
+    text: "little",
+  },
+  {
+    text: "few",
+  },
+];
--- pages/api/schedule/index.ts
+++ pages/api/schedule/index.ts
@@ -1,20 +1,32 @@
+import dayjs from "dayjs";
+
 import { runExamStatsJob } from "@/lib/schedules/exam";
 import { runIncorrectParagraphsStatsJob } from "@/lib/schedules/incorrect-paragraphs";
 import { ensureLogin, resp } from "@/lib/utils";
+import { df } from "@/utils";
 
 let timer = null;
+function run() {
+  runExamStatsJob();
+  runIncorrectParagraphsStatsJob();
+  if (timer !== null) {
+    clearInterval(timer);
+  }
+  timer = setInterval(async () => {
+    await Promise.all([runExamStatsJob(), runIncorrectParagraphsStatsJob()]);
+    console.log(df(dayjs()));
+    console.log("/* ----------- SCHEDULE JOB RUN COMPLETED ---------------*/");
+  }, 1000 * 5 * 60);
+}
 
 export default async function provideJobService(req, res) {
   await ensureLogin(req, res);
 
   if (timer !== null) {
-    return resp("定时任务已经启动", res);
+    run();
+    return resp("定时任务重新启动", res);
   }
 
-  timer = setInterval(async () => {
-    await Promise.all([runExamStatsJob(), runIncorrectParagraphsStatsJob()]);
-    console.log("/* ----------- SCHEDULE JOB RUN COMPLETED ---------------*/");
-  }, 1000 * 5 * 60);
-
+  run();
   resp("定时任务启动成功", res);
 }
