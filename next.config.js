e0d1ed54fd5ea2a6b578d3c11cc4157636dc9b43
李涛 <litaowork@aliyun.com>
2023-03-07 13:32:23 +0800

    chore: don't upload js file to qiniu

--- next.config.js
+++ next.config.js
@@ -3,10 +3,10 @@ const path = require("path");
 
 const WindiWebpackPlugin = require("windicss-webpack-plugin");
 
-const commit = cp.execSync('git log -1 --pretty=format:"%H"');
+// const commit = cp.execSync('git log -1 --pretty=format:"%H"');
 
 module.exports = {
-  assetPrefix: process.env.prod ? `//static.funzm.com/${commit}/` : "",
+  // assetPrefix: process.env.prod ? `//static.funzm.com/${commit}/` : "",
   swcMinify: true,
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Important: return the modified config
